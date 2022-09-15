af6f98618118fa5ce4d4e5931c37452be14382f5
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-16 01:37:46 +0530

    Time: 21 ms (92.79%), Space: 42.4 MB (91.02%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,21 +1,20 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int[] arr = new int[100001];
-        for(int i:nums){
-            if(i%2==0)
-                arr[i]++;
-        }
         
-        int max = -1 , ans =0;
+        int[] arr=new int [100001];
+        for(int i:nums){
+            if(i%2==0){
+                arr[i]++;
+            }
+        }
+        int ans=0 , max =-1;
         for(int i=0;i<100001;i++){
             if(arr[i]>max){
                 max=arr[i];
                 ans=i;
             }
         }
-        // if even number is not present
-        if(max==0)
-            return -1;
+        if(max==0) return -1;
         return ans;
     }
 }
