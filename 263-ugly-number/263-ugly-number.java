7e4c99a55711bedcb5f9dfa5bdfc97a3ceeded4b
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-16 16:10:30 +0530

    Time: 2 ms (69.69%), Space: 41.2 MB (54.59%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean isUgly(int n) {
-        
-        if(n < 1) { return false; }
-        if(n < 7) { return true; }
-    
-        while(n % 2 == 0) {
-            n /= 2;
+        if(n<=0) return false;
+        while(n>1)
+        {
+            if(n%2==0)
+                n/=2;
+           else if(n%3==0)
+                n/=3;
+           else if(n%5==0)
+                n/=5;
+            else
+                return false;
         }
-    
-        while(n % 3 == 0) {
-            n /= 3;
-        }
-    
-        while(n % 5 == 0) {
-            n /= 5;
-        }
-    
-        return n == 1 ? true : false; 
+        return true;
     }
 }
 
