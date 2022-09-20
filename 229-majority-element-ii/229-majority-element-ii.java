5008403716f103a1a9bc6ee70611e9d030237b0d
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-20 15:35:48 +0530

    Time: 3 ms (85.03%), Space: 49.5 MB (72.19%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,16 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        List<Integer> list = new ArrayList<>();
-        for(Map.Entry m:map.entrySet()){
-            if((int)m.getValue()>n/3){
-                list.add((int)m.getKey());
+        int n1 = -1 , n2 = -1 , c1 = 0 , c2 = 0 , n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==n1)
+                c1++;
+            else if(nums[i]==n2)
+                c2++;
+            else if(c1==0){
+                n1 = nums[i];
+                c1=1;
+            }else if(c2==0){
+                n2 = nums[i];
+                c2 = 1;
+            }else{
+                c1--;
+                c2--;
             }
         }
+        
+        List<Integer> list = new ArrayList<>();
+        c1 = 0;
+        c2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==n1)
+                c1++;
+            else if(nums[i]==n2)
+                c2++;
+        }
+        if(c1>n/3)
+            list.add(n1);
+        if(c2>n/3)
+            list.add(n2);
         return list;
+        
     }
 }
