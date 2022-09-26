43262c677fbed58c26c601d64bc3e201714c04f1
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-26 12:08:47 +0530

    Time: 79 ms (47.58%), Space: 44.3 MB (62.32%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -3,8 +3,8 @@ class Solution {
         int n = nums.length;
         if(n==0) return 0;
         int[] dp = new int[n];
-        dp[0]=1;
-        int ans =1;
+        dp[0] = 1;
+        int ans = 1;
         for(int i=1;i<n;i++){
             int max = 0;
             for(int j=0;j<i;j++){
@@ -12,7 +12,7 @@ class Solution {
                     max = Math.max(max,dp[j]);
                 }
             }
-            dp[i]= max + 1;
+            dp[i]=max+1;
             ans = Math.max(ans,dp[i]);
         }
         return ans;
