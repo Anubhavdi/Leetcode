2a7d6c1081d72ed3d3d5d4fefe806e11db51ff8c
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-26 23:10:11 +0530

    Time: 1841 ms (5.03%), Space: 68.5 MB (66.91%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -1,41 +1,41 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         
-        if(k<=1) return 0;
-        int n = nums.length;
-        int count=0;
-        int left = 0;
-        int prod = 1;
-        for(int i=0;i<n;i++){
-            prod *= nums[i];
-            while(prod>=k){
-                prod /= nums[left++];
-            }
-            count += i-left+1;
-        }
-        return count;
-        
-        
-        
-        
-        
-        
-        
-        
+        // if(k<=1) return 0;
         // int n = nums.length;
-        // if(n<=1) return 0;
-        // int count =0;
+        // int count=0;
+        // int left = 0;
         // int prod = 1;
         // for(int i=0;i<n;i++){
-        //     prod = nums[i];
-        //     if(prod<k)
-        //         count++;
-        //     for(int j=i+1;j<n && prod<k;j++){
-        //         prod *= nums[j];
-        //         if(prod<k)
-        //             count++;
+        //     prod *= nums[i];
+        //     while(prod>=k){
+        //         prod /= nums[left++];
         //     }
+        //     count += i-left+1;
         // }
         // return count;
+        
+        
+        
+        
+        
+        
+        
+        
+        int n = nums.length;
+        if(n<=1) return 0;
+        int count =0;
+        int prod = 1;
+        for(int i=0;i<n;i++){
+            prod = nums[i];
+            if(prod<k)
+                count++;
+            for(int j=i+1;j<n && prod<k;j++){
+                prod *= nums[j];
+                if(prod<k)
+                    count++;
+            }
+        }
+        return count;
     }
 }
