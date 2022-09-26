b5eacf0c258c80b14780b144a7bbe16f0c849185
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-26 09:47:29 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (95.93%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int ans = Integer.MAX_VALUE;
+        for(char ch='a';ch<='z';ch++){
+            int index = s.indexOf(ch);
+            if(index!=-1&&index==s.lastIndexOf(ch))
+                ans = Math.min(ans,index);
+        }
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
+}
