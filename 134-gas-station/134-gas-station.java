b63317e98332dc4f40642693ead1f6df51b1711e
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-09-27 04:35:27 +0530

    Time: 4 ms (49.40%), Space: 81.5 MB (12.47%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int pos = -1;
-        int cur = 0;
-        int total = 0;
-        for(int i=0;i<gas.length;i++){
-            int diff = gas[i]-cost[i];
-             cur += diff;
-            total += diff;
-            if(cur<0){
-                cur =0;
-                pos = i;
-            }
-            
-        }
-        if(total>=0){
-            return pos+1;
-        }
-        return -1;
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int start = 0;
+    int leftSum = 0;
+    int rightSum = 0;
+	
+    for (int i = 0; i < gas.length; i++) {
+      if (rightSum < 0) {
+        start = i;
+        leftSum += rightSum;
+        rightSum = 0;
+      }
+      rightSum += gas[i] - cost[i];
     }
+	
+    return leftSum + rightSum < 0 ? -1 : start; 
+  }
 }
