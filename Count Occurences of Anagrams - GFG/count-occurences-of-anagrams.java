ad0d74bac21c2058c35fb7bd2207a46451f2b1f3
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2022-11-16 01:30:29 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.java
@@ -27,48 +27,49 @@ class GFG {
 
 class Solution {
 
-    int search(String pat, String txt) {
+    int search(String p, String s) {
         // code here
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < pat.length(); i++) {
-            char ch = pat.charAt(i);
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
+        //List<Integer> list = new ArrayList<>();
+        int i=0,j=0;
+        int k=p.length();
+        int n = s.length();
+        int ans =0;
+        
+        Map<Character,Integer> map = new HashMap<>();
+        for(int a=0;a<k;a++){
+            map.put(p.charAt(a),map.getOrDefault(p.charAt(a),0)+1);
         }
-        int count = map.size();
-        int k = pat.length();
-        int i = 0, j = 0, ans = 0;
-        while (j < txt.length()) {
-            char end = txt.charAt(j);
-            if (map.containsKey(end)) {
-                map.put(end, map.get(end) - 1);
-
-                if (map.get(end) == 0) {
+        int count=map.size();
+        while(j<n){
+            
+            if(map.containsKey(s.charAt(j))){
+                map.put(s.charAt(j),map.get(s.charAt(j))-1);
+                if(map.get(s.charAt(j))==0){
                     count--;
                 }
             }
+            
 
-            if ((j - i + 1) < k) {
+            if(j-i+1<k){
                 j++;
-            } else if ((j - i + 1) == k) {
-                if (count == 0) {
+            }else if(j-i+1==k){
+                
+                if(count==0){
+                    //list.add(i);
                     ans++;
                 }
-                char start = txt.charAt(i);
-                if (map.containsKey(start)) {
-                    map.put(start, map.get(start) + 1);
-                    if (map.get(start) == 1) {
+                if(map.containsKey(s.charAt(i))){
+                    map.put(s.charAt(i),map.get(s.charAt(i))+1);
+                    if(map.get(s.charAt(i))==1){
                         count++;
                     }
                 }
+                
                 i++;
                 j++;
             }
         }
+        //System.out.print(ans);
         return ans;
     }
 }
