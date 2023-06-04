e84ce5e5cfa6e59c7ffea6a099f44f47df21f4f1
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-06-04 13:38:57 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().count(arr, n, x);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int start = search(arr,n,x,true);
+        int end = search(arr,n,x,false);
+        if(start==-1 || end == -1) return 0;
+        return end - start+1;
+    }
+    public int search(int[] nums,int n,int target,boolean isthere){
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = (start+end)>>1;
+            if(target<nums[mid]){
+                end = mid-1;
+            }else if(target>nums[mid]){
+                start = mid+1;
+            }else{
+                ans = mid;
+                if(isthere){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+}
