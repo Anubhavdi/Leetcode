b534d8f794f1844dc335688af2707239d3762230
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-07-10 12:43:24 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (89.32%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,10 +1,16 @@
 class Solution {
     public int findComplement(int num) {
         
-        if(num==0) return 1;
-        int x = (int)(Math.log(num)/Math.log(2))+1;
-        int mask = (1<<x)-1;
-        return num^mask;
+        // if(num==0) return 1;
+        // int x = (int)(Math.log(num)/Math.log(2))+1;
+        // int mask = (1<<x)-1;
+        // return num^mask;
+        
+        int n = 0;
+        while(n<num){
+            n = (n<<1)|1;
+        }
+        return n^num;
         
     }
 }
