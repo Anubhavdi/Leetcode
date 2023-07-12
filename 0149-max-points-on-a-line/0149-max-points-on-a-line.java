0c350958dda0263e78139a6a8ef80470922b24c9
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-07-12 23:23:02 +0530

    Time: 23 ms (68.03%), Space: 43.5 MB (60.33%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,29 +1,29 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int n = points.length;
-        if(n==1 || n==2)return n;
+//         int n = points.length;
+//         if(n==1 || n==2)return n;
     
-      int ans = 0;
-      Map<Double, Integer> hm = new HashMap<>();
-      for(int [] p1:points){
-         int count=0; 
-         for(int[] p2:points){
-            if(p2[0] == p1[0] && p2[1] == p1[1]){
-              count++;
-              continue;
-            }
-            double slope = (double)(p2[1] - p1[1])/(double)(p2[0] - p1[0]);
-            hm.put(slope, hm.getOrDefault(slope, 0)+1);
-            ans = Math.max(ans,hm.get(slope)+count);
+//       int ans = 0;
+//       Map<Double, Integer> hm = new HashMap<>();
+//       for(int [] p1:points){
+//          int count=0; 
+//          for(int[] p2:points){
+//             if(p2[0] == p1[0] && p2[1] == p1[1]){
+//               count++;
+//               continue;
+//             }
+//             double slope = (double)(p2[1] - p1[1])/(double)(p2[0] - p1[0]);
+//             hm.put(slope, hm.getOrDefault(slope, 0)+1);
+//             ans = Math.max(ans,hm.get(slope)+count);
             
-         }
-        //  for(int values : hm.values())
-        //   ans = Math.max(ans, values+count);
+//          }
+//         //  for(int values : hm.values())
+//         //   ans = Math.max(ans, values+count);
            
-          hm.clear(); 
-      }
+//           hm.clear(); 
+//       }
       
-      return ans;
+//       return ans;
         
         
         // int n = p.length;
@@ -62,23 +62,24 @@ class Solution {
         // return ans+1;
         
         
-        // int ans = 0;
-        // for (int[] p1:points){
-        //     Map<Double, Integer> check = new HashMap<>();
-        //     for (int[]  p2:points){
-        //         if(p1==p2){
-        //             continue;
-        //         }
-        //         double slope;
-        //         if (p1[0] == p2[0])
-        //             slope = Double.MAX_VALUE;
-        //         else{
-        //             slope = (p2[1] - p1[1]) / ((double) (p2[0] - p1[0]));
-        //         }
-        //         check.put(slope, check.getOrDefault(slope, 0) + 1);
-        //         ans = Math.max(ans, check.get(slope));
-        //     }
-        // }
-        // return ans+1;
+        int ans = 0;
+        for (int[] p1:points){
+            Map<Double, Integer> check = new HashMap<>();
+            for (int[]  p2:points){
+                if(p1==p2){
+                    continue;
+                }
+                double slope;
+                slope = (p2[1] - p1[1]) / ((double) (p2[0] - p1[0]));
+                // if (p1[0] == p2[0])
+                //     slope = Double.MAX_VALUE;
+                // else{
+                //     slope = (p2[1] - p1[1]) / ((double) (p2[0] - p1[0]));
+                // }
+                check.put(slope, check.getOrDefault(slope, 0) + 1);
+                ans = Math.max(ans, check.get(slope));
+            }
+        }
+        return ans+1;
     }
 }
