aab8fa2a1899d7282fdce7cb73fbb55d06146346
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-07-12 22:30:17 +0530

    Time: 21 ms (79.47%), Space: 43.4 MB (64.70%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,19 +1,38 @@
 class Solution {
-    public int maxPoints(int[][] p) {
-        int n = p.length;
-        if(n<=2) return n;
-        int max = 2;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int cur = 0;
-                for(int k=0;k<n;k++){
-                    if(((p[j][1]-p[i][1])*(p[i][0]-p[k][0])) == ((p[i][1]-p[k][1])*(p[j][0]-p[i][0]))){
-                        cur++;
-                    }
+    public int maxPoints(int[][] points) {
+        // int n = p.length;
+        // if(n<=2) return n;
+        // int max = 2;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int cur = 0;
+        //         for(int k=0;k<n;k++){
+        //             if(((p[j][1]-p[i][1])*(p[i][0]-p[k][0])) == ((p[i][1]-p[k][1])*(p[j][0]-p[i][0]))){
+        //                 cur++;
+        //             }
+        //         }
+        //         max = Math.max(max,cur);
+        //     }
+        // }
+        // return max;
+        
+        
+        int ans = 0;
+        for (int i = 0; i<points.length; ++i){
+            Map<Double, Integer> check = new HashMap<>();
+            for (int  j = i+1; j<points.length; ++j){
+                double slope;
+                if (points[i][0] == points[j][0])
+                    slope = Double.MAX_VALUE;
+                else{
+                    slope = (points[i][1] - points[j][1]) / ((double) (points[i][0] - points[j][0]));
+                    if (slope == -0.0)
+                        slope = 0.0;
                 }
-                max = Math.max(max,cur);
+                check.put(slope, check.getOrDefault(slope, 0) + 1);
+                ans = Math.max(ans, check.get(slope));
             }
         }
-        return max;
+        return ans+1;
     }
 }
