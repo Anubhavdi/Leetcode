76278a3de814e05764c2fe0c31c93478670b2298
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-07-19 23:22:02 +0530

    Time: 1 ms (91.06%), Space: 46.4 MB (38.01%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,20 +14,35 @@
  * }
  */
 class Solution {
-    int max = Integer.MIN_VALUE;
+    //int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-
-        helper(root);
-        return max;
+        
+        
+        int[] dp = new int[1];
+        dp[0] = Integer.MIN_VALUE;
+        ht(root,dp);
+        return dp[0];
+        
+        // helper(root);
+        // return max;
         
     }
-    int helper(TreeNode root){
-        if(root == null) return 0;
-
-        int left = Math.max(helper(root.left),0);
-        int right = Math.max(helper(root.right),0);
-
-        max = Math.max(max,root.val+left+right);
-        return root.val + Math.max(left,right);
+    
+    int ht(TreeNode root,int[] dp){
+        if(root==null) return 0;
+        int lh = Math.max(ht(root.left,dp),0);
+        int rh = Math.max(ht(root.right,dp),0);
+        dp[0] = Math.max(dp[0],root.val+lh+rh);
+        return root.val+Math.max(lh,rh);
     }
+    
+//     int helper(TreeNode root){
+//         if(root == null) return 0;
+
+//         int left = Math.max(helper(root.left),0);
+//         int right = Math.max(helper(root.right),0);
+
+//         max = Math.max(max,root.val+left+right);
+//         return root.val + Math.max(left,right);
+//     }
 }
