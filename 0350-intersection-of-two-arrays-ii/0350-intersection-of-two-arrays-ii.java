f9bae46cc7317b8ef6cf2914f3f339928f55478a
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-08-21 22:51:12 +0530

    Time: 10 ms (5.45%), Space: 43.5 MB (44.83%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,24 +1,41 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length){
-            intersect(nums2,nums1);
-        }
+//         if(nums1.length>nums2.length){
+//             intersect(nums2,nums1);
+//         }
         
-        int[] freq = new int[1001];
+//         int[] freq = new int[1001];
+//         for(int i:nums1){
+//             freq[i] += 1;
+//         }
+        
+//         int[] res = new int[nums1.length];
+//         int j=0;
+//         for(int i:nums2){
+//             if(freq[i]>0){
+//                 res[j++] = i;
+//                 freq[i] -= 1;
+//             }
+//         }
+        
+        
+//         return Arrays.copyOf(res,j);
+        
+        
+        List<Integer> list = new ArrayList<>();
         for(int i:nums1){
-            freq[i] += 1;
+            list.add(i);
         }
         
-        int[] res = new int[nums1.length];
-        int j=0;
+        int[] res = new int [nums1.length];
+        int j = 0;
         for(int i:nums2){
-            if(freq[i]>0){
+            if(list.contains(i)){
                 res[j++] = i;
-                freq[i] -= 1;
+                list.remove(Integer.valueOf(i));
+                
             }
         }
-        
-        
         return Arrays.copyOf(res,j);
         
     }
