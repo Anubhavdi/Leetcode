735cff61900120c7576b1e3f5c021c14af84a830
ANUBHAV DIXIT <73746079+24323090018@users.noreply.github.com>
2023-08-21 22:40:50 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (49.99%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length>nums2.length){
+            intersect(nums2,nums1);
+        }
+        
+        int[] freq = new int[1001];
+        for(int i:nums1){
+            freq[i] += 1;
+        }
+        
+        int[] res = new int[nums1.length];
+        int j=0;
+        for(int i:nums2){
+            if(freq[i]>0){
+                res[j++] = i;
+                freq[i] -= 1;
+            }
+        }
+        
+        
+        return Arrays.copyOf(res,j);
+        
+    }
+}
